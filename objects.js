37b1f9bff74ce7c3e060f137c24f9c3e729c2533
Paul Elion <paul.j.elion@gmail.com>
2018-08-07 22:37:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist, AFI, Death){
   playlist["AFI"] = "Death"
   return playlist
 }
+
+function removeFromPlaylist(playlist, AFI){
+  delete playlist.AFI;
+  return playlist
+}
